da2e033cb013abeb68e65dcde038b1dfd32bb5a5
Saran <romansaran446@gmail.com>
2021-01-16 12:13:31 +0530

    named export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import index from "../actions";
+import index from './actions';
 import App from "./Components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+    
